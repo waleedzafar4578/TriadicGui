542894825b47ca2ba98eadd8e065edbbe1a3c987
Jawad-Farrukh <144983753+Jawad-Farrukh@users.noreply.github.com>
2024-05-26 01:34:18 +0500

    Changes in Overview Design. Content of Overview For Later

--- src/App.jsx
+++ src/App.jsx
@@ -57,8 +57,8 @@ function App() {
     //           <Connection onLogin={handleLogin} toggleRender = {isCreatingAccount} toggleFunction = {handleCreatingAccount}/>
     //       )}
     //   </>
-    // <Overview />
-    <EditorPage/>
+    <Overview />
+    // <EditorPage/>
   )
 }
 
---
+++ src/assets/overviewContent.js
@@ -0,0 +1,6 @@
+const overViewContent = {
+    introduction: {
+        content: "Welcome to Triadic SQL DB! Triadic SQL DB is web-based SQL engine that  addresses the challenge of imperfect information in database engine and offers an alternative to traditional binary and fuzzy logic-based systems."
+    }
+};
+export default overViewContent;
